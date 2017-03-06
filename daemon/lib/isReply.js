146949205a6425f4044d2403ab687d77fdaa85b2
Sandro Padin <sandropadin@gmail.com>
2017-03-05 22:12:22 -0800

    Also check for null

--- daemon/lib/isReply.js
+++ daemon/lib/isReply.js
@@ -1,5 +1,5 @@
 function isReply({in_reply_to_status_id_str} = {}) {
-  return in_reply_to_status_id_str !== undefined;
+  return in_reply_to_status_id_str !== undefined && in_reply_to_status_id_str !== null;
 }
 
 module.exports = isReply;
